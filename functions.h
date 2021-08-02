caf9a6bd9b917d4ec43429bfea00e8a48ca23f9a
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-08-02 15:01:48 -0400

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-//struct of an image
+// struct of an image
 typedef struct {
     
     char *type;
@@ -18,51 +18,28 @@ typedef struct {
 
 extern image_t image;
 
-//function to alloc a matrix of n X m
 
 int **alloc_matrix(int n, int m);
 
-//function to load an image
-
 void load_function(image_t *image, char *file_name);
-
-//function to load a pgm image in ascii format
 
 void load_pgm_asc(image_t *image, char *file_name);
 
-//function to load a pgm image in binary format
-
 void load_pgm_bin(image_t *image, char *file_name);
-
-//function to save an image with binary format
 
 void save_bin_function(image_t *image, char *file_name);
 
-//function to save an image with ascii format
-
 void save_asc_function(image_t *image, char *file_name);
-
-//function to select_all_coordonates
 
 void select_all (image_t *image);
 
-//function to select all_size of image
-
 void select_all_function(image_t *image);
-
-//function to select size of image
 
 void select_function(image_t *image, int x1, int y1, int x2, int y2);
 
-//swap two integers
-
 void swap(int *number1, int *number2);
 
-//function to crop an image
-
 void crop_function(image_t *image);
-
-//functions to rotate an image 
 
 void rotate_function(image_t *image, char *reg);
 
